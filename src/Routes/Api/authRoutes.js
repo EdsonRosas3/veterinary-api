a52818e0580cd6648d0451316e3338a1b94359f7
EdsonRosas3 <edsonrosas321@gmail.com>
2021-04-23 19:01:53 -0400

    nuevo cambio

--- src/Routes/Api/authRoutes.js
+++ src/Routes/Api/authRoutes.js
@@ -1,7 +1,7 @@
 const {Router} = require('express');
 const router =  Router();
 const {verifySignup} = require('../../Middleware');
-const {signIn, signUp} = require('../../controllers/authController');
+const {signIn, signUp} = require('../../Controllers/veterinaryController');
 
 router.post('/signin',signIn);
 router.post('/signup',verifySignup.checkDuplicateEmail,signUp);
